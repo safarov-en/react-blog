3a18131a2598f64cc2a8ffcdc4debb7d3be0702a
safarov-en <elkhan.safarov@domru.ru>
2024-04-25 20:33:36 +0500

    Added a menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import TopBar from "./topbar/topbar";
+
 function App() {
   return (
-    <div className="App">
-      blog app
-    </div>
+    <>
+      <TopBar />
+    </>
   );
 }
 
---
+++ src/topbar/topbar.jsx
@@ -0,0 +1,27 @@
+import './topbar.css';
+
+export default function TopBar() {
+    return (
+        <div className="top">
+            <div className="top-left">
+                <i className="top-icon fa-brands fa-vk"></i>
+                <i className="top-icon fa-brands fa-square-twitter"></i>
+                <i className="top-icon fa-brands fa-square-pinterest"></i>
+                <i className="top-icon fa-brands fa-square-instagram"></i>
+            </div>
+            <div className="top-center">
+                <ul className="top-list">
+                    <li className="top-list-item">HOME</li>
+                    <li className="top-list-item">ABOUT</li>
+                    <li className="top-list-item">CONTACT</li>
+                    <li className="top-list-item">WRITE</li>
+                    <li className="top-list-item">LOGOUT</li>
+                </ul>
+            </div>
+            <div className="top-right">
+                <img className="top-img" src="https://goo.su/UlxfAE" alt="" />
+                <i className="top-search-icon fa-solid fa-magnifying-glass"></i>
+            </div>
+        </div>
+    )
+}
