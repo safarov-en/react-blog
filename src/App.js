9879fc8453e20b73afa210916cf2aaad84b7de7b
safarov-en <safarov.elkhan110@gmail.com>
2024-04-26 21:51:33 +0500

    Adding routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import TopBar from "./components/topbar/topbar";
-import Login from "./pages/login/login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/home/home';
 import Register from "./pages/register/register";
+import Login from "./pages/login/login";
+import Write from './pages/write/write';
+import Settings from './pages/settings/settings';
+import Single from './pages/single/single';
+import TopBar from "./components/topbar/topbar";
 
 function App() {
+  const user = false;
   return (
-    <>
+    <Router>
       <TopBar />
-      <Register />
-    </>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
 import Profile from '../../img/profile.jpg';
 import './topbar.css';
 
 export default function TopBar() {
+    const user = false;
     return (
         <div className="top">
             <div className="top-left">
@@ -12,15 +14,30 @@ export default function TopBar() {
             </div>
             <div className="top-center">
                 <ul className="top-list">
-                    <li className="top-list-item">HOME</li>
-                    <li className="top-list-item">ABOUT</li>
-                    <li className="top-list-item">CONTACT</li>
-                    <li className="top-list-item">WRITE</li>
-                    <li className="top-list-item">LOGOUT</li>
+                    <li className="top-list-item"><Link className="link" to="/">HOME</Link></li>
+                    <li className="top-list-item"><Link className="link" to="/">ABOUT</Link></li>
+                    <li className="top-list-item"><Link className="link" to="/">CONTACT</Link></li>
+                    <li className="top-list-item"><Link className="link" to="/write">WRITE</Link></li>
+                    <li className="top-list-item">
+                        {user && 'LOGOUT'}
+                    </li>
                 </ul>
             </div>
             <div className="top-right">
-                <img className="top-img" src={Profile} alt="" />
+                {
+                    user ? 
+                        ( <img className="top-img" src={Profile} alt="" />) :
+                        (
+                            <ul className="top-list">
+                                <li className="top-list-item">
+                                    <Link className="link" to="/login">LOGIN</Link>
+                                </li>
+                                <li className="top-list-item">
+                                    <Link className="link" to="/register">REGISTER</Link>
+                                </li>
+                            </ul>
+                        )
+                }
                 <i className="top-search-icon fa-solid fa-magnifying-glass"></i>
             </div>
         </div>
